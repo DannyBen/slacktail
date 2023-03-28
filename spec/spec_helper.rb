fc10ce20132cb4ecc2720531c8038635c4dd2422
Danny Ben Shitrit <db@dannyben.com>
2023-03-28 14:31:52 +0000

    add basic integration tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,11 @@ require_relative 'spec_mixin'
 # Consistent Colsole output (for rspec_fixtures)
 ENV['TTY'] = 'on'
 
+ORIGINAL_SLACK_API_TOKEN = ENV['SLACK_API_TOKEN']
+
 RSpec.configure do |c|
   c.include SpecMixin
+
+  # Do not run specs marked with :local in CI
+  c.filter_run_excluding :local if ENV['CI']
 end
