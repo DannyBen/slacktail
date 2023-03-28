69794c18eec9f7fa5b9efebd08d6b5f25d5e335a
Danny Ben Shitrit <db@dannyben.com>
2023-03-28 14:40:03 +0000

    improve integration test

--- spec/slacktail/integration_spec.rb
+++ spec/slacktail/integration_spec.rb
@@ -7,6 +7,7 @@ describe Client, :local do
     ENV['SLACK_API_TOKEN'] = ORIGINAL_SLACK_API_TOKEN
     described_class.default = nil
     described_class.default.start_async
+    sleep 5  # wait for cache population
   end
 
   describe '#users' do
@@ -17,14 +18,16 @@ describe Client, :local do
   end
 
   describe '#channels' do
-    it 'returns a hash' do
+    it 'returns a hash of Channel objects' do
       expect(subject.channels).to be_a Hash
+      expect(subject.channels.values.first).to be_a Slack::RealTime::Models::Channel
     end
   end
 
   describe '#bots' do
-    it 'returns a hash' do
+    it 'returns a hash of Bot objects' do
       expect(subject.bots).to be_a Hash
+      expect(subject.bots.values.first).to be_a Slack::RealTime::Models::Bot
     end
   end
 end
